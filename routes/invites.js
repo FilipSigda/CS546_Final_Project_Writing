45448656373849b86805b31ef775b09abcaeec80
war3397 <russop2202@yahoo.com>
2024-12-16 20:26:15 -0500

    small update

---
+++ routes/invites.js
@@ -0,0 +1,16 @@
+/*import { Router } from "express";
+//import data from '../data/stories.js';
+
+const router = Router();
+
+
+//AJAX requests
+router.route('/group').post( async (req,res) => {
+    
+    let recent = await data.getMostRecent(5);
+    let views = await data.getHighestViews(5);
+
+    
+});
+
+export default router;*/
